564720754f3e50e2f9531bab436e85fd64130bad
Gaurav SinghaRoy <gaurav@tiatros.com>
2013-10-14 13:13:56 +0200

    included stocks charts

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ App = Ember.Application.create();
 App.Router.map(function() {
   // put your routes here
 });
-
+var categories = new Array();
+var data = new Array();
 
 if($("#data_container").length <= 0){
 	$.getJSON( "https://api.mongolab.com/api/1/databases/qslab/collections/words?apiKey=fqhK5jNPNPCnv4aIBADT3l5Y0P2DMWJr&l=2000", function( data ) {
@@ -20,61 +21,35 @@ if($("#data_container").length <= 0){
 	  });
 
 
-	$(function () {
-		var categories = {};
-		$(".hdn-data-container").each(function(index){
 
-		});
-        $('#charts_container_index').highcharts({
-            title: {
-                text: 'Total Keystrokes per day',
-                x: -20 //center
-            },
-            subtitle: {
-                text: 'July',
-                x: -20
-            },
-            xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            },
-            yAxis: {
-                title: {
-                    text: 'Temperature (°C)'
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                valueSuffix: '°C'
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
-            },
-            series: [{
-                name: 'Tokyo',
-                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-            }, {
-                name: 'New York',
-                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-            }, {
-                name: 'Berlin',
-                data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-            }, {
-                name: 'London',
-                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-            }]
-        });
-    });
 
     
+		$(function() {
 
+			$.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function(data) {
+				// Create the chart
+				$('#charts_container_index').highcharts('StockChart', {
+					
+
+					rangeSelector : {
+						selected : 1
+					},
+
+					title : {
+						text : 'AAPL Stock Price'
+					},
+					
+					series : [{
+						name : 'AAPL',
+						data : data,
+						tooltip: {
+							valueDecimals: 2
+						}
+					}]
+				});
+			});
+
+		});
 
 
 
@@ -83,4 +58,4 @@ if($("#data_container").length <= 0){
 	});
 }
 
-      
+
