ea79a00d0ec612afc91c3b2cce4a981b2be8772a
Gaurav SinghaRoy <gaurav@tiatros.com>
2013-10-14 11:20:03 +0200

    getting the data

--- js/app.js
+++ js/app.js
@@ -4,8 +4,24 @@ App.Router.map(function() {
   // put your routes here
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+
+
+
+
+
+var items = ["one","bootshake"];
+$.getJSON( "https://api.mongolab.com/api/1/databases/qslab/collections/words?apiKey=fqhK5jNPNPCnv4aIBADT3l5Y0P2DMWJr&l=2000", function( data ) {
+   	$("#img_wait_index").hide();
+  $.each( data, function( key, val ) {
+  	var html = "";
+   	html += "<li>"
+   	$.each( val, function(k,v){
+   		if(html != "<li>")
+   			html+=", ";
+   		html += k + ":" + v;
+   	});	
+   	html += "</li>" ;
+   	$("#ul_index").append(html);
+  });
+
 });
