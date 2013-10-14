f5d55b0ae73d41d147f60ab0dad29003b749cff1
Gaurav SinghaRoy <gaurav@tiatros.com>
2013-10-14 13:40:35 +0200

    added highstock charts

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ App = Ember.Application.create();
 App.Router.map(function() {
   // put your routes here
 });
-var categories = new Array();
-var data = new Array();
+
+var total_data = new Array();
 
 if($("#data_container").length <= 0){
 	$.getJSON( "https://api.mongolab.com/api/1/databases/qslab/collections/words?apiKey=fqhK5jNPNPCnv4aIBADT3l5Y0P2DMWJr&l=2000", function( data ) {
@@ -20,36 +20,35 @@ if($("#data_container").length <= 0){
 	   	$("#data_container").append(html);
 	  });
 
-
-
-
+	  $(".hdn-data-container").each(function(index){
+	  	var date = new Date.parse($(this).attr("timestamp").substring(0,10));
+	  	var total_keystrokes = parseInt($(this).attr("total_keystrokes"));
+	  	total_data.push([date.getTime(),total_keystrokes]);
+	  });
     
 		$(function() {
 
-			$.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function(data) {
-				// Create the chart
+
 				$('#charts_container_index').highcharts('StockChart', {
 					
 
-					rangeSelector : {
-						selected : 1
-					},
+
 
 					title : {
-						text : 'AAPL Stock Price'
+						text : 'Total Keystrokes'
 					},
 					
 					series : [{
-						name : 'AAPL',
-						data : data,
+						name : 'Total Keystrokes',
+						data : total_data,
 						tooltip: {
-							valueDecimals: 2
+							valueDecimals: 0
 						}
 					}]
 				});
 			});
 
-		});
+
 
 
 
